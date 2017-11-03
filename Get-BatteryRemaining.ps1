77ffe0d84a7578aec64763f38cca908dbbe0feba
Tim Small <smalls1652@gmail.com>
2017-11-03 18:24:32 -0400

    Initial creation

---
+++ Get-BatteryRemaining.ps1
@@ -0,0 +1,26 @@
+function Get-BatteryRemaining
+{
+    <#
+
+.SYNOPSIS
+This script gathers information about battery power on a macOS machine and outputs it into the console.
+
+.DESCRIPTION
+This script gathers information about battery power on a macOS machine and outputs it into the console. It uses pmset as the data source for battery data.
+
+.EXAMPLE
+./HelloWorld.ps1
+
+#>
+    begin
+    {
+        $pmsetOut = (pmset -g batt).Split(";")
+    }
+
+    process
+    {
+        $powerObj = @{"Source" = $pmsetOut[0].Trim("Now drawing from").Trim("'") ; "Status" = (Get-Culture).TextInfo.ToTitleCase($pmsetOut[2].Trim()) ; "Remaining" =  $pmsetOut[3].Trim(" remaining present: true")}
+    
+        return [pscustomobject]$powerObj | Select-Object -Property "Source","Status","Remaining" | Format-Table -AutoSize
+    }
+}
