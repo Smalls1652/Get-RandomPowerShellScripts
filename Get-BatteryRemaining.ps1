e7e1b34b4eb587729a9d7986622b50c009125282
Tim Small <smalls1652@gmail.com>
2018-04-23 20:58:20 -0400

    Added a switch parameter to output the status as text. Still buggy.

--- Get-BatteryRemaining.ps1
+++ Get-BatteryRemaining.ps1
@@ -1,5 +1,4 @@
-function Get-BatteryRemaining
-{
+function Get-BatteryRemaining {
     <#
 
 .SYNOPSIS
@@ -12,15 +11,68 @@ This script gathers information about battery power on a macOS machine and outpu
 Get-BatteryRemaining
 
 #>
-    begin
-    {
+
+    param(
+        [switch]$BasicOutput
+    )
+
+    begin {
         $pmsetOut = (pmset -g batt).Split(";")
     }
 
-    process
-    {
-        $powerObj = @{"Source" = $pmsetOut[0].Trim("Now drawing from").Trim("'") ; "Status" = (Get-Culture).TextInfo.ToTitleCase($pmsetOut[2].Trim()) ; "Remaining" =  $pmsetOut[3].Trim(" remaining present: true")}
-    
-        return [pscustomobject]$powerObj | Select-Object -Property "Source","Status","Remaining" | Format-Table -AutoSize
+    process {
+        $powerObj = @{"Source" = $pmsetOut[0].Trim("Now drawing from").Trim("'") ; "Status" = (Get-Culture).TextInfo.ToTitleCase($pmsetOut[2].Trim()) ; "Remaining" = $pmsetOut[3].Trim(" remaining present: true")}
+
+        if ($powerObj.Remaining -eq "(no estimate)" -or $powerObj.Remaining -eq "ot ch" -or $powerObj.Remaining -eq "0:00") {
+            Write-Warning "Charging source recently changed. Battery time still calculating."
+        }
+        else {
+            if ($BasicOutput) {
+
+                $hoursRemain = $powerObj.Remaining.split(":")[0]
+                [int32]$minutesRemain = $powerObj.Remaining.split(":")[1]
+
+                [string]$hourOut = $null
+                [string]$minuteOut = $null
+
+                if ($hoursRemain -ne "0") {
+                    if ($hoursRemain -eq "1") {
+                        $hourOut = "$hoursRemain hour"
+                    }
+                    else {
+                        $hourOut = "$hoursRemain hours"
+                    }
+                }
+                else {
+                    $hourOut = $null
+                }
+
+                if ($minutesRemain -ne "0") {
+                    if ($minutesRemain -eq "1") {
+                        $minuteOut = "$minutesRemain minute"
+                    }
+                    else {
+                        $minuteOut = "$minutesRemain minutes"
+                    }
+                }
+                else {
+                    $minuteOut = $null
+                }
+
+                if ($minuteOut -ne $null -and $hourOut -ne $null) {
+                    return "There's an estimated $hourOut and $minuteOut remaining on your battery."
+                }
+                elseif ($minuteOut -ne $null -and $hourOut -eq $null) {
+                    return "There's an estimated $minuteOut remaining on your battery."
+                }
+                elseif ($minuteOut -eq $null -and $hourOut -ne $null) {
+                    return "There's an estimated $hourOut remaining on your battery."
+                }
+
+            }
+            else {
+                return [pscustomobject]$powerObj | Select-Object -Property "Source", "Status", "Remaining" | Format-Table -AutoSize
+            }
+        }
     }
 }
