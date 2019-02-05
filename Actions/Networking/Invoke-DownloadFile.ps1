628b0caf36910e5fcac1f8a13ec9038c242ed031
smalls1652 <smalls.online@icloud.com>
2019-02-04 20:56:15 -0500

    Updated Invoke-DownloadFile.ps1
    - Added error handling of the download.
    - Added in automatic URL parsing for filename (Defaults to the current working directory).
    - Added Verbose messages.

--- Actions/Networking/Invoke-DownloadFile.ps1
+++ Actions/Networking/Invoke-DownloadFile.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true)][string]$Url,
-    [Parameter(Mandatory = $true)][string]$Path
+    [string]$Path
 )
 
 function convertFileSize {
@@ -20,45 +20,100 @@ function convertFileSize {
     }
 }
 
-#Load in the WebClient object and create a temporary file to download to.
-$Downloader = New-Object -TypeName System.Net.WebClient
+Write-Verbose "URL set to ""$($Url)""."
+
+if (!($Path)) {
+    Write-Verbose "Path parameter not set, parsing Url for filename."
+    $URLParser = $Url | Select-String -Pattern ".*\:\/\/.*\/(.*\.{1}\w*).*" -List
+
+    $Path = "./$($URLParser.Matches.Groups[1].Value)"
+}
+
+Write-Verbose "Path set to ""$($Path)""."
+
+#Load in the WebClient object.
+Write-Verbose "Loading in WebClient object."
+try {
+    $Downloader = New-Object -TypeName System.Net.WebClient
+}
+catch [Exception] {
+    Write-Error $_ -ErrorAction Stop
+}
+
+#Creating a temporary file.
 $TmpFile = New-TemporaryFile
+Write-Verbose "TmpFile set to ""$($TmpFile)""."
 
 try {
 
     #Start the download by using WebClient.DownloadFileTaskAsync, since this lets us show progress on screen.
+    Write-Verbose "Starting download..."
     $FileDownload = $Downloader.DownloadFileTaskAsync($Url, $TmpFile)
 
     #Register the event from WebClient.DownloadProgressChanged to monitor download progress.
+    Write-Verbose "Registering the ""DownloadProgressChanged"" event handle from the WebClient object."
     Register-ObjectEvent -InputObject $Downloader -EventName DownloadProgressChanged -SourceIdentifier WebClient.DownloadProgressChanged | Out-Null
 
     #Wait two seconds for the registration to fully complete
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
+
+    if ($FileDownload.IsFaulted) {
+        Write-Verbose "An error occurred. Generating error."
+        Write-Error $FileDownload.GetAwaiter().GetResult()
+        break
+    }
 
     #While the download is showing as not complete, we keep looping to get event data.
     while (!($FileDownload.IsCompleted)) {
-        $EventData = Get-Event -SourceIdentifier WebClient.DownloadProgressChanged | Select-Object -ExpandProperty "SourceEventArgs"
 
-        $ReceivedData = ($EventData | Select-Object -ExpandProperty "BytesReceived" -Last 1)
-        $TotalToReceive = ($EventData | Select-Object -ExpandProperty "TotalBytesToReceive" -Last 1)
-        $TotalPercent = $EventData | Select-Object -ExpandProperty "ProgressPercentage" -Last 1
+        if ($FileDownload.IsFaulted) {
+            Write-Verbose "An error occurred. Generating error."
+            Write-Error $FileDownload.GetAwaiter().GetResult()
+            break
+        }
+
+        $EventData = Get-Event -SourceIdentifier WebClient.DownloadProgressChanged | Select-Object -ExpandProperty "SourceEventArgs" -Last 1
+
+        $ReceivedData = ($EventData | Select-Object -ExpandProperty "BytesReceived")
+        $TotalToReceive = ($EventData | Select-Object -ExpandProperty "TotalBytesToReceive")
+        $TotalPercent = $EventData | Select-Object -ExpandProperty "ProgressPercentage"
 
         Write-Progress -Activity "Downloading File" -Status "Percent Complete: $($TotalPercent)%" -CurrentOperation "Downloaded $(convertFileSize -bytes $ReceivedData) / $(convertFileSize -bytes $TotalToReceive)" -PercentComplete $TotalPercent
     }
 }
+catch [Exception] {
+    $ErrorDetails = $_
+
+    switch ($ErrorDetails.FullyQualifiedErrorId) {
+        "ArgumentNullException" { 
+            Write-Error -Exception "ArgumentNullException" -ErrorId "ArgumentNullException" -Message "Either the Url or Path is null." -Category InvalidArgument -TargetObject $Downloader -ErrorAction Stop
+        }
+        "WebException" {
+            Write-Error -Exception "WebException" -ErrorId "WebException" -Message "An error occurred while downloading the resource." -Category OperationTimeout -TargetObject $Downloader -ErrorAction Stop
+        }
+        "InvalidOperationException" {
+            Write-Error -Exception "InvalidOperationException" -ErrorId "InvalidOperationException" -Message "The file at ""$($Path)"" is in use by another process." -Category WriteError -TargetObject $Path -ErrorAction Stop
+        }
+        Default {
+            Write-Error $ErrorDetails -ErrorAction Stop
+        }
+    }
+}
 finally {
     #Cleanup tasks
+    Write-Verbose "Cleaning up..."
     Write-Progress -Activity "Downloading File" -Completed
     Unregister-Event -SourceIdentifier WebClient.DownloadProgressChanged
 
-    $Downloader.CancelAsync()
-
-    if ($FileDownload.IsCompleted) {
+    if (($FileDownload.IsCompleted) -and !($FileDownload.IsFaulted)) {
         #If the download was finished without termination, then we move the file.
+        Write-Verbose "Moved the downloaded file to ""$($Path)""."
         Move-Item -Path $TmpFile -Destination $Path -Force
     }
     else {
         #If the download was terminated, we remove the file.
+        Write-Verbose "Cancelling the download and removing the tmp file."
+        $Downloader.CancelAsync()
         Remove-Item -Path $TmpFile -Force
     }
 
